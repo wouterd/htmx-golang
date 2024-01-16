af69634dd88419598987ffbe241e3cb61f074e1f
Wouter Danes <wouter.danes@gmail.com>
2024-01-16 23:36:32 +0100

    Added completing of tasks and completed tasks are at the bottom

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 	"wouterd/playground/htmx/task"
 
@@ -15,12 +16,24 @@ type TaskInsert struct {
     Task string `json:"task"`
 }
 
-func main() {
-    r := mux.NewRouter()
+type TaskPatch struct {
+    Name *string `json:"name"`
+    Completed *bool `json:"completed"`
+}
 
-    tasks := task.Tasks{}
+func (patch TaskPatch) toStr() string {
+    s := "{"
+    if patch.Name != nil {
+        s += " Name=" + *patch.Name
+    }
+    if patch.Completed != nil {
+        s += " Completed=" + strconv.FormatBool(*patch.Completed)
+    }
+    return s + " }"
+}
 
-    r.Path("/tasks").Methods(http.MethodPost).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func taskCreateHandler(tasks *task.Tasks) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
         var taskJson TaskInsert
         if r.Body == nil {
             w.WriteHeader(http.StatusBadRequest)
@@ -39,12 +52,99 @@ func main() {
             Created: time.Now().UTC(),
             Completed: false,
         }
-        tasks.Add(task)
+        addedTask := tasks.Add(task)
         w.Header().Add("Content-Type", "text/html")
         w.WriteHeader(http.StatusOK)
-        fmt.Fprint(w, "<li>", taskJson.Task, "</li>")
-    })
+        template := task_list_item(&addedTask)
+        template.Render(context.Background(), w)
+    }
+}
+
+func taskUpdateHandler(tasks *task.Tasks) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        fmt.Println("INCOMING PATCH!")
+        vars := mux.Vars(r)
+        id, err := strconv.Atoi(vars["id"])
+        if err != nil {
+            fmt.Printf("ID is not an int! (ID=%d)\n", id)
+            w.WriteHeader(http.StatusInternalServerError)
+            return
+        }
+
+        task, err := tasks.Get(id)
+        if err != nil {
+            w.WriteHeader(http.StatusNotFound)
+            return
+        }
+        
+        var taskJson TaskPatch
+        if r.Body == nil {
+            w.WriteHeader(http.StatusBadRequest)
+            fmt.Fprintln(w, "No body found, can't patch")
+            return
+        }
+
+        decodeErr := json.NewDecoder(r.Body).Decode(&taskJson)
+        if decodeErr != nil {
+            w.WriteHeader(http.StatusBadRequest)
+            fmt.Fprintln(w, "No valid JSON body")
+            return
+        }
+
+        fmt.Println("incoming task: ", taskJson.toStr())
+        fmt.Println("current task: ", task)
+        if taskJson.Name != nil {
+            task.Name = *taskJson.Name
+        }
+        if taskJson.Completed != nil {
+            task.Completed = *taskJson.Completed
+        }
+        fmt.Println("Merged task: ", task)
+        tasks.Update(id, task)
+    }
+}
+
+func taskUpdateFormHandler(tasks *task.Tasks) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        fmt.Println("INCOMING PATCH VIA FORM!")
+        vars := mux.Vars(r)
+        id, err := strconv.Atoi(vars["id"])
+        if err != nil {
+            fmt.Printf("ID is not an int! (ID=%d)\n", id)
+            w.WriteHeader(http.StatusInternalServerError)
+            return
+        }
+
+        task, err := tasks.Get(id)
+        if err != nil {
+            w.WriteHeader(http.StatusNotFound)
+            return
+        }
+
+        task.Completed = r.PostFormValue("completed") != ""
+        tasks.Update(id, task)
+
+        template := tasks_list(tasks.Tasks())
+        err = template.Render(context.Background(), w)
+        if err != nil {
+            fmt.Println(err.Error())
+            w.WriteHeader(http.StatusInternalServerError)
+        }
+    }
+}
+
+func main() {
+    fmt.Println("Big F")
+    r := mux.NewRouter()
+
+    tasks := task.Tasks{}
+
+    r.Path("/tasks").Methods(http.MethodPost).HandlerFunc(taskCreateHandler(&tasks))
  
+    r.Path("/tasks/{id:[0-9]+}").Methods(http.MethodPatch).Headers("Content-Type", "application/json").HandlerFunc(taskUpdateHandler(&tasks))
+
+    r.Path("/tasks/{id:[0-9]+}").Methods(http.MethodPatch).Headers("Content-Type", "application/x-www-form-urlencoded").HandlerFunc(taskUpdateFormHandler(&tasks))
+
     r.Path("/").Methods(http.MethodGet).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         template := index(tasks.Tasks())
         err := template.Render(context.Background(), w)
--- task/task.go
+++ task/task.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -8,36 +9,85 @@ type Task struct {
     Name string
     Created time.Time
     Completed bool
+    Id int
 }
 
 type Tasks struct {
     tasks []Task
 }
 
-func (list *Tasks) Add(task Task) {
+func (list *Tasks) Add(task Task) Task {
+    task.Id = len(list.tasks)
     list.tasks = append(list.tasks, task)
+    return task
 }
 
-func (list Tasks) Tasks() TaskIterator {
+func (list *Tasks) Tasks() TaskIterator {
+    tasks := list.tasks[0:len(list.tasks)]
     return TaskIterator{
-        curr: len(list.tasks),
-        tasks: list.tasks[0:len(list.tasks)],
+        currUncompleted: len(list.tasks),
+        currCompleted: len(list.tasks),
+        tasks: tasks,
+        length: len(list.tasks),
     }
+}
+
+func (list *Tasks) Get(id int) (Task, error) {
+    if id >= len(list.tasks) {
+        return Task{}, fmt.Errorf("Can't find task with ID %d", id)
+    }
+    return list.tasks[id], nil
+}
+
+func (list *Tasks) Update(id int, task Task) error {
+    if id >= len(list.tasks) {
+        return fmt.Errorf("Can't find task with ID %d", id)
+    }
+    list.tasks[id] = task
+    return nil
 }
 
 type TaskIterator struct {
-    curr int
+    currUncompleted int
+    currCompleted int
     tasks []Task
+    length int
 }
 
-func (it *TaskIterator) Next() *Task {
-    it.curr--
-    if it.curr < 0 {
-        return nil
+func (it *TaskIterator) nextCompleted() *Task {
+    it.currCompleted--
+    for it.currCompleted >= 0 && !it.tasks[it.currCompleted].Completed {
+        it.currCompleted--
     }
-    return &(it.tasks[it.curr])
+    if it.currCompleted >= 0 {
+        return &it.tasks[it.currCompleted]
+    }
+    return nil
+}
+
+func (it *TaskIterator) nextUnCompleted() *Task {
+    it.currUncompleted--
+    for it.currUncompleted >= 0 && it.tasks[it.currUncompleted].Completed {
+        it.currUncompleted--
+    }
+    if it.currUncompleted >= 0 {
+        return &it.tasks[it.currUncompleted]
+    }
+    return nil
+}
+
+
+func (it *TaskIterator) Next() *Task {
+    next := it.nextUnCompleted()
+    if next == nil {
+        next = it.nextCompleted()
+    }
+    if next != nil {
+        it.length--
+    }
+    return next
 }
 
 func (it *TaskIterator) HasNext() bool {
-    return it.curr > 0
+    return it.length > 0
 }
